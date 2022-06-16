5b6e9f1d52393e9c0534283696cb846fe1068fc4
Boris Huezo <huezo.dd@gmail.com>
2022-06-15 21:16:06 -0600

    First commit

---
+++ config/release.config.js
@@ -0,0 +1,28 @@
+const namePackage = 'locale-region-list';
+const srcRoot = `.`;
+
+module.exports = {
+  extends: 'release.config.base.js',
+  pkgRoot: srcRoot,
+  tagFormat: namePackage + '-v${version}',
+  commitPaths: [`${srcRoot}/*`],
+  plguins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: `${srcRoot}/CHANGELOG.md`,
+      }
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: [`${srcRoot}/package.json`, `${srcRoot}/CHANGELOG.md`],
+        message: `release(version): Release ${namePackage}` +
+        '${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      }
+    ]
+  ]
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+
+const getLocaleAndRegion = (lang) => {
+    let obj = {
+        locale: '',
+        region: ''
+    }
+    if (!lang) {
+        return {
+            locale: null,
+            region: null
+        }
+    }
+    const [locale, region] = lang.split('-')
+    if (region) {
+        obj.region = region
+    } 
+    obj.locale = locale
+
+    return obj
+}
+
+
+const main = (languageSelected) => {
+    let result = {
+        regions: [],
+        locales: []
+    }
+
+    const { locale, region } = getLocaleAndRegion(languageSelected)
+    if (region) {
+        result.regions.push(region)
+    }
+    if (locale) {
+        result.locales.push(locale)
+    }
+    if (typeof navigator !== 'undefined') {
+        const languages = navigator.languages
+        const data = languages.reduce((obj, lang) => {
+            const { locale, region } = getLocaleAndRegion(lang)
+            if (region) {
+                obj.regions.push(region)
+            }
+            if (locale) {
+                obj.locales.push(locale)
+            }
+            return obj
+        }, {
+            regions: [],
+            locales: []
+        })
+        result.regions = [ ...result.regions, ...data.regions]
+        result.locales = [...result.locales, ...data.locales]
+    }
+    
+    result.locales.push('en')
+
+    result.locales = [...new Set(result.locales)]
+    result.regions = [...new Set(result.regions)]
+
+    return result
+}
+
+module.exports = main
